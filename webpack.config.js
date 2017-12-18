2124ea30989e356b904de8156f9606f78e43e213
ajayrnair <anair@walmartlabs.com>
2017-12-18 14:30:29 -0800

    Making it a runnable project

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,59 @@
-const path = require("path");
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        /* List of npm modules that must go into vendor bundle.
+        Check CommonsChunkPlugin to avoid duplicate modules in vendor and app bundles
+        */
+        bundle: './src/index.js',
+        vendor: ['react', 'redux'],
+    },
     output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "bundle.js"
+        path: path.join(__dirname, 'dist'),
+        //Chunk hash used for cache busting, Add manifest in CommonsChunkPlugin!!
+        filename: '[name].[chunkhash].js',
     },
     module: {
         rules: [{
-            use: "babel-loader",
-            test: /\.js$/
-        }, {
-            use: ExtractTextPlugin.extract({
-                use: 'css-loader'
-            }),
-            test: /\.css$/
-        }, {
-            test: /\.(jpg|jpeg)/,
-            use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 40000
-                    }
-                },
-                'image-webpack-loader'
-            ]
-        }],
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+            },
+            {
+                //You can use style-loader instead of ExtractTextPlugin to inject css as a style tag into html document
+                //use: ['style-loader', 'css-loader']
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader',
+                }),
+                test: /\.css$/,
+            },
+            {
+                test: /\.(jpg|jpeg)/,
+                use: [{
+                        loader: 'url-loader',
+                        options: {
+                            limit: 40000,
+                        },
+                    },
+                    'image-webpack-loader',
+                ],
+            },
+        ],
     },
     plugins: [
-        new ExtractTextPlugin('styles.css')
-    ]
+        new ExtractTextPlugin('styles.css'),
+        //Commons Chunk Plugin to specify not to duplicate the modules
+        //between vendor and app bundle. If common put it in the vendor module
+        //as mentioned in the names array
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest'],
+        }),
+        //Use src/index.html as template and add all the bundles in a script tag automatically
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
 };
